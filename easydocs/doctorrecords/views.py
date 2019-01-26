c5e0aa362b679d85eb70c97c32d5c76d2e17a10d
Walker Hildebrand <walker99113@gmail.com>
2019-01-26 18:48:33 -0500

    Update homepage

--- easydocs/doctorrecords/views.py
+++ easydocs/doctorrecords/views.py
@@ -2,16 +2,24 @@
 from django.shortcuts import render
 
 import datetime
+from . import models
 
 
 # Create your views here.
 def homepage(request):
+    hcpList = models.HCP.objects.all()
 
     now = datetime.datetime.now()
 
+    if(request.GET.get('btn_searchHCP')):
+        hcp = request.GET.get('textbox_HCP')
+    else:
+        hcp = "Not found..."
 
+    
     return render(request, 'home.html',
     {
-        'todayYYYYMMDD':          now.strftime("%Y-%m-%d"),
-        'todayWeekday':         now.strftime("%A"),               
+        'todayYYYYMMDD':            now.strftime("%Y-%m-%d"),
+        'todayWeekday':             now.strftime("%A"),
+        'hcpList':                  hcpList
     })
