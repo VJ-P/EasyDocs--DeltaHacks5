e4e80194c9738ad9977d32760eb37a52065ad2fe
Ali Shah Imtiaz <imtiaza@mcmaster.ca>
2019-01-26 17:25:38 -0500

    Added date and time for patients

--- easydocs/doctorrecords/admin.py
+++ easydocs/doctorrecords/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.HCP)
+admin.site.register(models.Medication)
+admin.site.register(models.Side_Effects)
+admin.site.register(models.Incompatible)
+admin.site.register(models.Condition)
+admin.site.register(models.System_Affected)
+admin.site.register(models.Patient)
--- easydocs/doctorrecords/models.py
+++ easydocs/doctorrecords/models.py
@@ -4,33 +4,77 @@ from django.db import models
 class HCP(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        verbose_name_plural = "HCP"
 
 class Medication(models.Model):
     name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Medication"
+
+class Date(models.Model):
+    start_date = models.DateField()
+    end_date= models.DateField()
 
 class Side_Effects(models.Model):
     side_effect = models.CharField(max_length=30)
     medication = models.ForeignKey('Medication', on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.side_effect
 
+    class Meta:
+        verbose_name_plural = "Side_Effects"
 
 class Incompatible(models.Model):
-    medication_1 = models.ForeignKey('Medication', on_delete = models.CASCADE)
-    medication_2 = models.ForeignKey('Medication', on_delete = models.CASCADE)
-
+    medication = models.ForeignKey('Medication', on_delete = models.CASCADE)
+    #medication_2 = models.ForeignKey('Medication', on_delete = models.CASCADE)
+    
+    class Meta:
+        verbose_name_plural = "Incompatible"
+    
+    def __str__(self):
+        return self.medication_1
+    
 class Condition(models.Model):
     name = models.CharField(max_length=30)
     medication = models.ForeignKey('Medication', on_delete = models.CASCADE)
-    prompt = models.ForeignKey('Prompt', on_delete = models.CASCADE)
+    #prompt = models.ForeignKey('Prompt', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Condition"
 
 class System_Affected(models.Model):
     system = models.CharField(max_length=30)
     condition = models.ForeignKey('Condition', on_delete = models.CASCADE)
-    prompt = models.ForeignKey('Prompt', on_delete = models.CASCADE)
-
-class Client(models.Model):
+    #prompt = models.ForeignKey('Prompt', on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.system
+    
+    class Meta:
+        verbose_name_plural = "System Affected"
+    
+class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     condition = models.ForeignKey( 'Condition', on_delete = models.CASCADE)
-
-class Prompt(models.Model):
-    message = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+    
+    class Meta:
+        verbose_name_plural = "Patient"
