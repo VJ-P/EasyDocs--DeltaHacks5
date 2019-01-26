ddc166446853a7221e34f38df40fc1ba817aa33d
Ali Shah Imtiaz <imtiaza@mcmaster.ca>
2019-01-26 16:31:29 -0500

    Fixed models

--- easydocs/doctorrecords/models.py
+++ easydocs/doctorrecords/models.py
@@ -10,43 +10,27 @@ class Medication(models.Model):
 
 class Side_Effects(models.Model):
     side_effect = models.CharField(max_length=30)
-    medication = models.ForeignKey(
-    'Medication',
-    on_delete = models.CASCADE)
+    medication = models.ForeignKey('Medication', on_delete = models.CASCADE)
 
 
 class Incompatible(models.Model):
-    medication_1 = models.ForeignKey(
-    'Medication',
-    on_delete = models.CASCADE)
-    medication_2 = models.ForeignKey(
-    'Medication',
-    on_delete = models.CASCADE)
+    medication_1 = models.ForeignKey('Medication', on_delete = models.CASCADE)
+    medication_2 = models.ForeignKey('Medication', on_delete = models.CASCADE)
 
 class Condition(models.Model):
     name = models.CharField(max_length=30)
-    medication = models.ForeignKey(
-    'Medication',
-    on_delete = models.CASCADE)
-    prompt = models.ForeignKey(
-    'Prompt',
-    on_delete = models.CASCADE)
+    medication = models.ForeignKey('Medication', on_delete = models.CASCADE)
+    prompt = models.ForeignKey('Prompt', on_delete = models.CASCADE)
 
 class System_Affected(models.Model):
     system = models.CharField(max_length=30)
-    condition = models.ForeignKey(
-    'Condition',
-    on_delete = models.CASCADE)
-    prompt = models.ForeignKey(
-    'Prompt',
-    on_delete = models.CASCADE)
+    condition = models.ForeignKey('Condition', on_delete = models.CASCADE)
+    prompt = models.ForeignKey('Prompt', on_delete = models.CASCADE)
 
 class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    condition = models.ForeignKey(
-    'Condition',
-    on_delete = models.CASCADE)
+    condition = models.ForeignKey( 'Condition', on_delete = models.CASCADE)
 
 class Prompt(models.Model):
     message = models.TextField()
