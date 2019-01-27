db61e632425d66355a88255bfecc4c8be3e0c488
Razi <syedru@mcmaster.ca>
2019-01-26 23:00:42 -0500

    testing query

--- easydocs/doctorrecords/tests.py
+++ easydocs/doctorrecords/tests.py
@@ -1,9 +1,10 @@
-from django.test import TestCase
+#from django.test import TestCase
 
 #import views
-from models import HCP
+#from models import HCP
+from doctorrecords import models
 
-
+print(models.Appointment.objects.all()[0])
 
 
 #print(views._todayYYYMMDD())
